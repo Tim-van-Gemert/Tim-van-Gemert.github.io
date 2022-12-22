d797adeb383b7489c1134a7ecc09f29646a7905b
Tim_van_Gemert <EMAIL_ADDRESS>
2022-12-22 14:00:32 +0100

    added socials for mobile

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,40 +2,47 @@
 // Randomized word script
 let changedWord = '';
 let targetWord = document.getElementById('softDev');
-const LETTERS = targetWord.innerText.split('')
-const ARR = targetWord.innerText.split('')
+const LETTERS = targetWord.innerText.split('');
+const ARR = targetWord.innerText.split('');
 
 function RandomizeLetters() {
-    const RANDOMIZED_WORD = []
+
+    const RANDOMIZED_WORD = [];
     ARR.forEach(element => {
         const random = Math.floor(Math.random() * LETTERS.length);
         RANDOMIZED_WORD.push(LETTERS[random]);
     });
+
     targetWord = RANDOMIZED_WORD.join('');
     document.getElementById('softDev').innerText = targetWord;
+
 }
 
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 async function asignCorrectLetter(arrRandomized) {
-    for (i = 0; i < ARR.length; i++) {
 
+    for (i = 0; i < ARR.length; i++) {
         arrRandomized[i] = ARR[i];
         result = arrRandomized.join('');
         document.getElementById('softDev').innerText = result;
 
+        console.log(result);
 
         await timer(70);
     }
+
 }
 
 
 function Execute() {
+
     RandomizeLetters();
     const arrRandomized = targetWord.split('');
     setTimeout(() => {
         asignCorrectLetter(arrRandomized);
     }, 1000);
+
 }
 
 Execute();
